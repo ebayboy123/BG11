b8e5857cee68b767af97558312d613dfb40d9ee4
cancel-cloud <Laggis_131@icloud.com>
2022-09-26 18:42:19 +0200

    feature: added correct way of calculating

--- Main.java
+++ Main.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double number1 = 0;
-        double number2 = 0;
+        double number1;
+        double number2;
+        String modifier;
 
 
         Scanner scanner = new Scanner(System.in);
@@ -21,6 +22,9 @@ public class Main {
         System.out.println("Zahl 2");
         String read2 = scanner.next();
 
+        System.out.println("Modifier or operator");
+        modifier = scanner.next();
+
         if (read2.isEmpty()) {
             number2 = 0.0;
         } else {
@@ -31,18 +35,35 @@ public class Main {
             System.out.println("Division nicht durchführbar!");
             System.exit(0);
         }
-        System.out.println(number1);
-        System.out.println(number2);
+
         double addition = number1 + number2;
         double substraktion = number1 - number2;
         float diff = (float) (number1 / (float) number2);
         double multiplication = number1 * number2;
 
         System.out.println("------------------------------------------------------------");
-        System.out.println("Das Ergebniss ist einer addition ist: " + addition);
-        System.out.println("Das Ergebniss ist einer subtraktion ist: " + substraktion);
-        System.out.println("Die Division ist: " + diff);
-        System.out.println("Die Multiplikation ist: " + multiplication);
+        switch (modifier) {
+            case "+" -> {
+                System.out.println("Das Ergebniss ist einer addition ist: " + addition);
+                break;
+            }
+            case "-" -> {
+                System.out.println("Das Ergebniss ist einer subtraktion ist: " + substraktion);
+                break;
+            }
+            case "*" -> {
+                System.out.println("Die Multiplikation ist: " + multiplication);
+                break;
+            }
+            case "/" -> {
+                System.out.println("Die Division ist: " + diff);
+                break;
+            }
+
+            default -> {
+                System.out.println("This isnt a correct operator, please use one of: '+'; '-'; '*'; '/'");
+            }
+        }
         System.out.println("------------------------------------------------------------");
 
     }
